ac28f46ec631573f16235a921b5b5734166d04ee
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-04-01 00:26:43 +0600

    final commit 31.03

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -9,19 +9,31 @@ export const Nav = () => {
             <ul className={s.navList}>
                 <li className={s.navListItem}>
                     <h4 className={s.navListItemTitle}>Home</h4>
-                    <img className={s.navListItemIcon} src='https://img.icons8.com/ios-filled/344/ffffff/home.png' alt={'home'}/>
+                    <span className={s.navIconWrapper}>
+                        <img className={s.navListItemIcon} src="https://img.icons8.com/ios-filled/344/ffffff/home.png"
+                             alt={'home'}/>
+                    </span>
                 </li>
                 <li className={s.navListItem}>
                     <h4 className={s.navListItemTitle}>About</h4>
-                    <img className={s.navListItemIcon} src='https://img.icons8.com/ios-filled/344/ffffff/home.png' alt={'home'}/>
+                    <span className={s.navIconWrapper}>
+                    <img className={s.navListItemIcon} src='https://img.icons8.com/ios-filled/344/ffffff/user.png'
+                         alt={'user'}/>
+                    </span>
                 </li>
                 <li className={s.navListItem}>
                     <h4 className={s.navListItemTitle}>Portfolio</h4>
-                    <img className={s.navListItemIcon} src='https://img.icons8.com/ios-filled/344/ffffff/home.png' alt={'home'}/>
+                    <span className={s.navIconWrapper}>
+                    <img className={s.navListItemIcon} src="https://img.icons8.com/ios-filled/344/ffffff/ammo-tin.png"
+                         alt={'case'}/>
+                    </span>
                 </li>
                 <li className={s.navListItem}>
                     <h4 className={s.navListItemTitle}>Contact</h4>
-                    <img className={s.navListItemIcon} src='https://img.icons8.com/ios-filled/344/ffffff/home.png' alt={'home'}/>
+                    <span className={s.navIconWrapper}>
+                    <img className={s.navListItemIcon} src="https://img.icons8.com/ios-filled/344/ffffff/new-post.png"
+                         alt={'message'}/>
+                    </span>
                 </li>
             </ul>
         </div>
