d0d7ea49a4927fae6b7439653b279b0ddcee8216
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-04-22 19:14:15 +0600

    added navlink to main

--- src/App.tsx
+++ src/App.tsx
@@ -11,49 +11,33 @@ import img from './components/Main/myPortrait.jpg';
 
 export function App() {
     const [mode, setMode] = useState<boolean>(true)
-
     const onChangeThemeHandler = () => {
         return setMode(!mode)
     }
+
+    let mainTheme = `${s.App} ${s.themeWhite}`;
+    let imgLogo = 'https://img.icons8.com/small/100/000000/moon-symbol.png'
+
+    if (mode) {
+        mainTheme = `${s.App} ${s.themeDark}`
+        imgLogo = 'https://img.icons8.com/ios/50/000000/sun--v1.png'
+    }
     return (
-        <div>
-            {mode
-                ? <div className={`${s.App} ${s.themeDark}`}>
-                    <Routes>
-                        <Route path="/*" element={<Main/>}/>
-                        <Route path="/home/*" element={<Main/>}/>
-                        <Route path="/about/*" element={<About/>}/>
-                        <Route path="/contact/*" element={<Contact/>}/>
-                        <Route path="/portfolio/*" element={<Portfolio/>}/>
-                    </Routes>
-                    <div className={s.switcherBtn}>
-                        <img className={s.mainBtnIcon}
-                             onClick={onChangeThemeHandler}
-                             src="https://img.icons8.com/ios/50/000000/sun--v1.png"
-                             alt={'Dark'}/>
-                    </div>
-                    <Nav/>
-                </div>
-                : <div className={`${s.App} ${s.themeWhite}`}>
-
-                    <Routes>
-                        <Route path="/*" element={<Main/>}/>
-                        <Route path="/home/*" element={<Main/>}/>
-                        <Route path="/about/*" element={<About/>}/>
-                        <Route path="/contact/*" element={<Contact/>}/>
-                        <Route path="/portfolio/*" element={<Portfolio/>}/>
-                    </Routes>
-                    <div className={s.switcherBtn}>
-                        <img className={s.mainBtnIcon}
-                             onClick={onChangeThemeHandler}
-                             src="https://img.icons8.com/small/100/000000/moon-symbol.png"
-                             alt={'White'}/>
-                    </div>
-                    <Nav/>
-                </div>
-            }
-
-
+        <div className={mainTheme}>
+            <Routes>
+                <Route path="/*" element={<Main/>}/>
+                <Route path="/home/*" element={<Main/>}/>
+                <Route path="/about/*" element={<About/>}/>
+                <Route path="/contact/*" element={<Contact/>}/>
+                <Route path="/portfolio/*" element={<Portfolio/>}/>
+            </Routes>
+            <div className={s.switcherBtn}>
+                <img className={s.mainBtnIcon}
+                     onClick={onChangeThemeHandler}
+                     src={imgLogo}
+                     alt={'logo'}/>
+            </div>
+            <Nav/>
         </div>
 
     );
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,40 +2,39 @@ import React from 'react';
 import s from './Main.module.css'
 import img from './myPortrait.jpg'
 import {GrayCurtain} from '../GrayCurtain';
+import {NavLink} from 'react-router-dom';
 
 export const Main = () => {
     return (
         <div className={s.mainContainer}>
             <GrayCurtain/>
             <div className={s.yellowBlock}/>
-
             <div className={s.container}>
                 <div className={s.mainPhotoWrapper}>
                     <div className={s.mainPhoto}/>
                     <div className={s.mainPhotoBG}/>
                 </div>
-
-
                 <div className={s.textContainer}>
                     <h2>I'm Drobotyuk Andrii.</h2>
                     <span>Front-end developer</span>
-                    <p>I'm a Ukrainian based web designer & front‑end developer focused on crafting clean &
-                        user‑friendly
-                        experiences, I am passionate about building excellent software that improves the lives of those
-                        around
-                        me.</p>
-                    <div className={s.btnWrapper}>
-                        <div className={s.mainBtn}>
-                            <div className={s.mainBtnTitle}>more about me</div>
-                            <div className={s.mainBtnCircle}>
-                                <img className={s.mainBtnIcon}
-                                     src="https://img.icons8.com/ios-filled/344/ffffff/right--v1.png"
-                                     alt={'home'}/>
+                    <p>I'm a Ukrainian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
+
+                        <NavLink to="/about/*" style={{textDecoration: 'none', width: '218px'}}>
+                            <div className={s.btnWrapper}>
+                            <div className={s.mainBtn}>
+                                <a className={s.mainBtnTitle}>more about me</a>
+                                <div className={s.mainBtnCircle}>
+                                    <img className={s.mainBtnIcon}
+                                         src="https://img.icons8.com/ios-filled/344/ffffff/right--v1.png"
+                                         alt={'home'}/>
+                                </div>
                             </div>
-                        </div>
+                            </div>
+                        </NavLink>
+
                     </div>
                 </div>
             </div>
-        </div>
+
     );
 };
