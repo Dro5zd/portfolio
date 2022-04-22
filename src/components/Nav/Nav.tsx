8e079e18ebc118ccef046d69073e121f9ffd2efb
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-04-23 00:53:23 +0600

    2204 finish commit3

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -7,7 +7,7 @@ export const Nav = () => {
         <nav className={s.navbar}>
             <NavbarRow title='Home' path={'/home/*'} icon={"https://img.icons8.com/ios-filled/344/ffffff/home.png"}/>
             <NavbarRow title='About' path={'/about/*'} icon={"https://img.icons8.com/ios-filled/344/ffffff/user.png"}/>
-            <NavbarRow title='Portfolio' path={'/portfolio/*'} icon={"https://img.icons8.com/ios-filled/344/ffffff/ammo-tin.png"}/>
+            <NavbarRow title='Portfolio' path={'/my_portfolio/*'} icon={"https://img.icons8.com/ios-filled/344/ffffff/ammo-tin.png"}/>
             <NavbarRow title='Contact' path={'/contact/*'} icon={"https://img.icons8.com/ios-filled/344/ffffff/new-post.png"}/>
         </nav>
     );
